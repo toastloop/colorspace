93de3e026e1b5c4e6c24886ea3978d66cdfe7a8b
toastloop <toastloop@gmail.com>
2022-06-10 09:55:24 -0600

    Modified playerctl to only control spotify/spotifyd

--- polybar/spotify.sh
+++ polybar/spotify.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" || exit ; pwd -P )"
 export SCRIPTPATH
 
-spotify=$(playerctl -p spotifyd,%any metadata --format "  {{ title }} - {{ artist }} " -s)
+spotify=$(playerctl --player=spotify,spotifyd metadata --format "  {{ title }} - {{ artist }} " -s)
 
 if [ -n "$spotify" ]; then
     echo "$spotify" | tee "$SCRIPTPATH/cache.txt"
